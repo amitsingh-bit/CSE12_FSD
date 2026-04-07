ee3073efe6df839881d21aea364e74402069d51b
amitsingh-bit <amitsinghme80521@gmail.com>
2026-04-07 12:41:32 +0530

    CSS MODULE EXAMPLE

--- Project 17/src/App.jsx
+++ Project 17/src/App.jsx
@@ -2,9 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className={StyleSheet.container}>
-      <h2>CSS MODULE EXAMPLE</h2>
-      
+    <div className="App">
+      <div className="box">
+        <h1>CSS MODULE EXAMPLE</h1>
+      </div>
     </div>
   );
 }
